4bd702c6f98f3ddf0d0ede451f99c92042de444d
Andrew Han <andrewhan@gmail.com>
2023-03-01 22:42:19 -0800

    Change types.

--- index.tsx
+++ index.tsx
@@ -1,29 +1,29 @@
-import React from "react";
+import React, { ReactNode, ReactElement } from "react";
 
 type PredicateProps = {
   condition: boolean;
-  children: JSX.Element;
-  or?: () => JSX.Element;
+  children: ReactNode;
+  or?: () => ReactNode;
 };
 
 export function Predicate({
   condition,
   children,
   or = () => <></>,
-}: PredicateProps): JSX.Element {
-  return condition ? children : or();
+}: PredicateProps): ReactElement {
+  return <>{condition ? children : or()}</>;
 }
 
 type NullableProps<T> = {
   value: T | null | undefined;
-  children: (t: T) => JSX.Element;
-  or?: () => JSX.Element;
+  children: (t: T) => ReactNode;
+  or?: () => ReactNode;
 };
 
 export function Nullable<T>({
   value,
   children,
   or = () => <></>,
-}: NullableProps<T>): JSX.Element {
-  return value !== null && value !== undefined ? children(value) : or();
+}: NullableProps<T>): ReactElement {
+  return <>{value !== null && value !== undefined ? children(value) : or()}</>;
 }
